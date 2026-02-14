1da4eb82ee9a7e68e63593b8c656139ca6291868
NAO5802 <i100185b@gmail.com>
2026-02-14 18:11:07 +0900

    error resolved

--- app/src/main/kotlin/org/example/core/MemoryBody.kt
+++ app/src/main/kotlin/org/example/core/MemoryBody.kt
@@ -1,12 +1,18 @@
 package org.example.core
 
+import java.io.InputStream
 import java.nio.ByteBuffer
 
 data class MemoryBody(override val payload: ByteBuffer) : Body {
     constructor(payload: String): this(payload.asByteBuffer())
+    override val length: Long
+        get() = payload.length().toLong()
+    override val stream: InputStream
+        get() = payload.array().inputStream(payload.position(),payload.length())
     // do nothing
     override fun close() {}
 }
 
 private fun ByteArray.asByteBuffer(): ByteBuffer = ByteBuffer.wrap(this)
 private fun String.asByteBuffer() : ByteBuffer = toByteArray().asByteBuffer()
+private fun ByteBuffer.length(): Int = limit() - position()
